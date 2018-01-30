758e8ddafdd5e4d7cb0d8db73ad2a1d7184954a8
php 08 <php.08@espeto.cec.ime.usp.br>
2018-01-30 21:44:52 -0200

    proj laravel

---
+++ resources/views/saudacao.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    @for($i=1;$i<=5;$i++)
+      <h1> Olá <?= $i ?> </h1>
+    @endfor
+  </body>
+</html>
